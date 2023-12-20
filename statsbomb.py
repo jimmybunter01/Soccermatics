4bd4a45aebd9a25324be968b2790f093579a7d51
James <65281539+jimmybunter01@users.noreply.github.com>
2023-12-20 12:39:10 +0000

    Some setup for being able to get the data automaitcally.

---
+++ statsbomb.py
@@ -0,0 +1,27 @@
+import requests
+from tqdm import tqdm
+
+def main():
+    article_ids = []
+    wyscout_collection_url = "https://api.figshare.com/v2/collections/4415000/articles"
+    download_urls = {}
+
+    api_response = requests.get(wyscout_collection_url)
+    for article in api_response.json():
+        if article["defined_type_name"] == "dataset":
+            article_ids.append(article["id"])
+
+    for article_id in article_ids:
+        wyscout_article_url = f"https://api.figshare.com/v2/articles/{article_id}"
+        api_response = requests.get(wyscout_article_url)
+        dataset_details = api_response.json()["files"][0]
+        dataset_name = dataset_details["name"]
+        download_urls[dataset_name] = dataset_details["download_url"]
+
+    for file, url in tqdm(download_urls.items()):
+        file_request = requests.get(url)
+        open(f"data/wyscout/{file}", "wb").write(file_request.content)
+
+
+if __name__ == "__main__":
+    main()
