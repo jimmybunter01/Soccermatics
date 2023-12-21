ed8a55af96c90db88e8a760283b164f9a357aa2d
jimmybunter01 <65281539+jimmybunter01@users.noreply.github.com>
2023-12-21 17:11:09 +0000

    Got all data needed,. wyscout script working and first notebook setup and the first visualisation plotted.

--- wyscout.py
+++ wyscout.py
@@ -1,10 +1,31 @@
 import requests
+import os
 from tqdm import tqdm
+
+def check_for_data_dir():
+    cwd = os.getcwd()
+    if 'data' not in os.listdir(cwd):
+        print("No data directory! Creating it!")
+        try: 
+            os.mkdir(f"{cwd}\\data\\wyscout")
+            print("Data Directory Made!")
+        except Exception as e:
+            print(f"Directory could not be made due  to - {e}")
+
+    if 'wyscout' not in os.listdir(f"{cwd}\\data"):
+        try: 
+            os.mkdir(f"{cwd}\\data\\wyscout")
+            print("Wyscout Directory Made!")
+        except Exception as e:
+            print(f"Directory could not be made due  to - {e}")
+
 
 def main():
     article_ids = []
     wyscout_collection_url = "https://api.figshare.com/v2/collections/4415000/articles"
     download_urls = {}
+
+    check_for_data_dir()
 
     api_response = requests.get(wyscout_collection_url)
     for article in api_response.json():
